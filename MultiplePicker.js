925332bbae0246a4fee9e311d4e11cee952bea60
Fuad <javadoffuad@gmail.com>
2018-04-02 00:22:53 +0300

    bugfix - dropdown open & hide

--- MultiplePicker.js
+++ MultiplePicker.js
@@ -45,31 +45,40 @@ export default class MultiplePicker extends Component {
 
     render() {
         const {open, data, count} = this.state;
-
-        console.warn('------', open)
-
         return (
-            <View>
-                <TouchableOpacity
-                    activeOpacity={.7}
-                    onPress={() => this.onOpen()}>
-                    <View style={styles.button}>
-                        <Text>{`Selected ${count}`}</Text>
-                    </View>
-                </TouchableOpacity>
-                <View style={[styles.dropdown, open ? {} : {display:'none'}]}>
-                    {
-                        data.map((item, index) =>
-                            <TouchableOpacity
-                                key={index}
-                                activeOpacity={.7}
-                                onPress={() => this.onSelect(item)}>
-                                <Text style={[styles.title, item.selected ? {color:'red'} : {}]}>
-                                    {item.title}
+            <View style={{flex: 1}}>
+                <View>
+                    <TouchableOpacity
+                        activeOpacity={.7}
+                        onPress={() => this.onOpen()}>
+                        <View style={styles.button}>
+                            <Text>{`Selected ${count}`}</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <View style={[styles.dropdown, open ? {} : {opacity:0}]}>
+                        {
+                            data.map((item, index) =>
+                                <TouchableOpacity
+                                    key={index}
+                                    activeOpacity={.7}
+                                    onPress={() => this.onSelect(item)}>
+                                    <Text style={[styles.title, item.selected ? {color:'red'} : {}]}>
+                                        {item.title}
+                                    </Text>
+                                </TouchableOpacity>
+                            )
+                        }
+                        <TouchableOpacity
+                            activeOpacity={.75}
+                            onPress={() => this.onOpen()}
+                            >
+                            <View style={[styles.buttonClose, count > 0 ? {backgroundColor: 'green'}: {}]}>
+                                <Text style={styles.buttonCloseText}>
+                                    {count > 0 ? 'Select' : 'Close'}
                                 </Text>
-                            </TouchableOpacity>
-                        )
-                    }
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         );
@@ -88,12 +97,24 @@ const styles = StyleSheet.create({
     },
     dropdown: {
         width: '100%',
-        backgroundColor: 'blue',
+        backgroundColor: '#fff',
         position: 'absolute',
         top: 50,
         zIndex: 3,
+       // opacity: 1,
     },
     title: {
-        padding: 15
+        padding: 15,
+        flexGrow: 1,
+    },
+    buttonClose: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        height: 50,
+        backgroundColor:'red'
+    },
+    buttonCloseText: {
+        color: '#fff',
     }
 });
